b07342ade6c46ac7a15b9a8f3ea11359d12fec0a
Rob Morgan <robbym@gmail.com>
2010-01-09 14:31:02 +0800

    initial commit

---
+++ pearhubtutorial.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *  Pearhub Tutorial Script.
+ *
+ *  @package    PearhubTutorial
+ *  @version    $Id$
+ */
+ 
+/**
+ *  Pearhub Tutorial Class.
+ *
+ *  @package PearhubTutorial
+ */
+class PearhubTutorial
+{
+    /**
+     * Simple Method that Returns 'Hello World'.
+     * 
+     * @return string
+     */
+    public static function getString()
+    {
+        return 'Hello World';
+    }
+}
